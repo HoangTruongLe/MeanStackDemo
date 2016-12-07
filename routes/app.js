9b2691cb6ce0e310928ea4ff368c2706396bb581
truong <htr.lee@gmail.com>
2016-12-07 12:09:05 +0700

    add some stupid lines of code

--- routes/app.js
+++ routes/app.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController')
 
+//Rendering index view.
 router.get('/', function (req, res, next) {
     res.render('index');
 });
-router.route('/test')
+// router.use('/',function (req,res,next) {
+//
+//     }
+// );
+//User routes.
+router.route('/user')
     .get(function (req,res) {
         userController.getAllUser(req,res);
     })
@@ -17,5 +23,14 @@ router.route('/test')
     })
     .delete(function (req,res) {
         userController.deleteUser(req,res);
-    })
+    });
+router.route('/user:id')
+    .get(function (req,res) {
+        var userId = req.param.id;
+        userController.getUserById(userId,res);
+    });
+router.route('/user/login')
+    .post(function (req,res) {
+        userController.login(req,res);
+    });
 module.exports = router;
